44a52d454cd4564f8e7a4ec6eb2034ba20eee78e
GraceC-326 <grace.a.cochran@maine.edu>
2025-05-01 10:26:03 -0400

    Updated MorseTree.java
    Made the functions for MorseTree. Note that the encoder and decoder are unfinished.

--- MorseTree.java
+++ MorseTree.java
@@ -1,3 +1,28 @@
 public class MorseTree {
     
+    public TreeNode<Character> root;
+
+    public MorseTree(){
+        root = new TreeNode<>(null);
+    }
+
+    public void morsePreorder(){
+        root.preOrder(root);
+    }    
+
+    public void morsePostorder(){
+        root.postOrder(root);
+    }    
+
+    public String encoder(String input){
+
+        return "a";
+    }
+
+    public String decoder(String input){
+        
+        
+        return "b";
+    }
+
 }
