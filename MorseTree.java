d400c0644bf8f6df90cd3ffc5dee750d0703f7d7
GraceC-326 <grace.a.cochran@maine.edu>
2025-05-02 12:13:58 -0400

    Updated MorseTree.java
    Filled out the decoding and encoding methods of the class.

--- MorseTree.java
+++ MorseTree.java
@@ -17,14 +17,60 @@ public class MorseTree {
     }    
 
     public String encoder(String input){
+        // letter
+        String finalStr = "";
+        for (int i = 0; i < input.length(); i++){
+            finalStr = finalStr + encodeChar(input.charAt(i), "", root);
+            finalStr = finalStr + "|";
+        }
+        return finalStr;
+    }
 
-        return "a";
+    public String encodeChar(char input, String output, TreeNode<Character> node){
+        // letter
+
+        if (node.getElement() == input){
+            return output;
+        }
+        else{
+            if (node.getLeft() == null && node.getRight() == null){
+                return "";
+            } else if (node.getLeft() != null && node.getRight() == null){
+                return encodeChar(input, output + "o", node.getLeft());
+            } else if (node.getLeft() == null && node.getRight() != null){
+                return encodeChar(input, output + "-", node.getRight());
+            } else{
+                return encodeChar(input, output + "o", node.getLeft()) + encodeChar(input, output + "-", node.getRight());
+            }
+        }
     }
 
     public String decoder(String input){
+        // for loop for entire string 
+        String[] splitStrings = input.split("/|");
+        String finalStr = "";
+        for (int i = 0; i < splitStrings.length; i++){
+            String temp = decodeChar(splitStrings[i]);
+            finalStr = finalStr + temp;
+        }
         
-        
-        return "b";
+        return finalStr;
     }
 
+    public String decodeChar(String input){
+        // if a dot, go left, if  a dash, go right.
+        // step thru the tree
+        
+        TreeNode<Character> temp = root;
+        for (int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == 'o'){
+                temp = temp.getLeft();
+            }
+            else if (input.charAt(i) == '-'){
+                temp = temp.getRight();
+            }
+        }
+
+        return temp.getElement() + "";
+    }
 }
